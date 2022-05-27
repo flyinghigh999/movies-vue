846f2a75fbbf45447485c5c90db21049baa53cf8
flyinghigh999 <1581696073@qq.com>
2022-05-27 20:00:22 +0800

    1、新增admin页面。2、完成login页到admin的跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Admin from '../views/AdminView.vue'
+import Login from '../views/LoginView.vue'
 
 const routes = [
   {
@@ -15,7 +17,12 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    component: Login
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin
   }
 ]
 
