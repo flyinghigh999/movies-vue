955ce666db3cb0065a3bd8cf23fab4fb6431e64a
flyinghigh999 <1581696073@qq.com>
2022-05-28 13:44:34 +0800

    使用mounted解决组件化样式的遗留问题

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: Login
   },
   {
     path: '/login',
